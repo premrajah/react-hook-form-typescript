23e3887324c70fe87d29e5903a9aa55da02e91f8
PremR <prem.rajah@gmail.com>
2023-07-24 14:56:47 +0200

    added based react-hook-form

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,15 +1,19 @@
+import { useForm } from 'react-hook-form';
 
 export default function YoutubeForm() {
+  const form = useForm();
+  const { register } = form;
+
   return (
     <form>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+      <label htmlFor='username'>Username</label>
+      <input type='text' id='username' {...register("username")} />
+      <label htmlFor='email'>Email</label>
+      <input type='email' id='email' {...register("email")} />
+      <label htmlFor='channel'>Channel</label>
+      <input type='text' id='channel' {...register("channel")} />
 
-        <button>Submit</button>
+      <button>Submit</button>
     </form>
-  )
+  );
 }
