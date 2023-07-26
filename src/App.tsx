1b92d74b1b4cea7f8005f18c8efb8c2466f2b091
PremR <prem.rajah@gmail.com>
2023-07-26 10:52:48 +0200

    setup basic hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import YoutubeForm from './components/YoutubeForm';
+import YupYoutubeForm from './components/YupYoutubeForm';
 
 function App() {
   return (
     <div>
-      <YoutubeForm />
+      {/* <YoutubeForm /> */}
+      <YupYoutubeForm />
     </div>
   );
 }
