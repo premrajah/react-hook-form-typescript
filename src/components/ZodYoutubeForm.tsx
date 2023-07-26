7f0914259221993d39e969395eda102299f284aa
PremR <prem.rajah@gmail.com>
2023-07-26 11:43:27 +0200

    intergration of zod with react-hook-form

--- src/components/ZodYoutubeForm.tsx
+++ src/components/ZodYoutubeForm.tsx
@@ -1,5 +1,13 @@
 import { useForm } from 'react-hook-form';
-import { DevTool } from "@hookform/devtools";
+import { DevTool } from '@hookform/devtools';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from 'zod';
+
+const schema = z.object({
+    username: z.string().nonempty("Username is required"),
+    email: z.string().nonempty("Email is required").email(),
+    channel: z.string().nonempty("Channel is required"),
+})
 
 type FromValues = {
   username: string;
@@ -13,11 +21,12 @@ export default function ZodYoutubeForm() {
       username: 'Superman',
       email: '',
       channel: '',
-    }
+    },
+    resolver: zodResolver(schema)
   });
 
-  const {register, handleSubmit, formState, control } = form;
-  const {errors} = formState;
+  const { register, handleSubmit, formState, control } = form;
+  const { errors } = formState;
 
   const handleOnFormSubmit = (data: FromValues) => {
     console.log('form ', data);
@@ -26,29 +35,25 @@ export default function ZodYoutubeForm() {
   return (
     <div className='formContainer'>
       <form onSubmit={handleSubmit(handleOnFormSubmit)} noValidate>
-
         <div className='form-control'>
           <label htmlFor='username'>Username</label>
-          <input type='text' id='username' {...register("username")} />
-          <p className="error">{errors.username?.message}</p>
+          <input type='text' id='username' {...register('username')} />
+          <p className='error'>{errors.username?.message}</p>
         </div>
         <div className='form-control'>
           <label htmlFor='email'>Email</label>
-          <input type='email'  id='email' {...register("email")} />
-          <p className="error">{errors.email?.message}</p>
-
+          <input type='email' id='email' {...register('email')} />
+          <p className='error'>{errors.email?.message}</p>
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <input type='text' id='channel' {...register("channel")} />
-          <p className="error">{errors.channel?.message}</p>
-
+          <input type='text' id='channel' {...register('channel')} />
+          <p className='error'>{errors.channel?.message}</p>
         </div>
 
         <div className='form-control'>
           <button>Submit</button>
         </div>
-
       </form>
       {/* end form */}
       <DevTool control={control} />
